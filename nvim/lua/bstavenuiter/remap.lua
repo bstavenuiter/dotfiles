2fed2db6f2a250afd68ece3a7dc50fd03d54cb7e
Bart Stavenuiter <b.stavenuiter@socialblue.com>
2023-05-10 15:01:53 +0200

    removing the custom todo check keymap

--- nvim/lua/bstavenuiter/remap.lua
+++ nvim/lua/bstavenuiter/remap.lua
@@ -63,32 +63,5 @@ keymap('i', ',,', '<ESC>:norm A,<CR>', opts)
 keymap("n", "<C-u>", "<C-u>zz", opts);
 keymap("n", "<C-d>", "<C-d>zz", opts);
 
-keymap("n", "<leader>bo", "<cmd>silent !open -a \"Brave Browser\" %<CR>", opts);
+keymap("n", "<leader>bo", "<cmd>silent !open -a \"Brave Browser\" \"%:p\"<CR>", opts);
 
--- checkbox todo
--- will add a single - when there isn't any present
--- will add " [ ]" when the line starts with a - but no "[ ]" is present
--- will check the box to "- [X]" when a "- [ ]" is present
--- will uncheck the box to "- [ ]" when a "- [X]" is present
-TodoCheck = function()
-    local bufnr = vim.api.nvim_get_current_buf();
-    local linenr = vim.api.nvim_win_get_cursor(0)[1];
-    local curline = vim.api.nvim_buf_get_lines(bufnr, linenr - 1, linenr, false)[1]
-    local _, t = string.find(curline, "^%s*-%s%[%s%]")
-    local _, s = string.find(curline, "^%s*-%s%[X%]")
-    local _, r = string.find(curline, "^%s*-")
-
-    if t ~= nil then
-        vim.api.nvim_buf_set_text(bufnr, linenr-1, t-5, linenr-1, t, {"- [X]"})
-    elseif s ~= nil then
-        vim.api.nvim_buf_set_text(bufnr, linenr-1, s-5, linenr-1, s, {"- [ ]"})
-    elseif r ~= nil then
-        local new_line = curline:gsub("^(%s*)-(.*)", "%1- [ ]%2")
-        vim.api.nvim_buf_set_text(bufnr, linenr-1, 0, linenr-1, curline:len(), { new_line })
-    else
-        local new_line = curline:gsub("^(%s*)(.*)", "%1- %2")
-        vim.api.nvim_buf_set_text(bufnr, linenr-1, 0, linenr-1, curline:len(), { new_line })
-    end
-end
-
-vim.api.nvim_set_keymap("n", "<C-Space>", "<cmd>lua TodoCheck()<cr>", { noremap = true } );
