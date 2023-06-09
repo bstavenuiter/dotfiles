e80a3b7f2d481b790aff3ae131054d827a5ba62b
Bart Stavenuiter <b.stavenuiter@socialblue.com>
2023-06-09 12:45:59 +0200

    change theme to onenord

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    theme = 'onedark',
+    theme = 'onenord',
     component_separators = '|',
     section_separators = '',
   },
