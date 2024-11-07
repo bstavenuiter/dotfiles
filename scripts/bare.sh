dd60178ece5af4f735fe19eb7bbd934433aa62cb
Bart Stavenuiter <spoiker@hotmail.com>
2024-11-07 09:02:56 +0100

    add 3rd option to give main branch name
    sometimes the main branch in called master

--- scripts/bare.sh
+++ scripts/bare.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 folder=$1
 repo=$2
+branch=${3:main}
 mkdir $folder
 cd $folder
 git clone --bare $repo .bare
 cd .bare 
-git worktree add ../main main
+git worktree add ../main $branch
 cd ../main
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
