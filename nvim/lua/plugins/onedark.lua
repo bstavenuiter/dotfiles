7f5c35543b844c223f1ebe1f2e0b59f1c416d4d2
Bart Stavenuiter <spoiker@hotmail.com>
2025-10-08 16:53:16 +0200

    one dark to be more in line with other plugins

--- nvim/lua/plugins/onedark.lua
+++ nvim/lua/plugins/onedark.lua
@@ -1,9 +1,10 @@
 return {
-		"olimorris/onedarkpro.nvim",
-		priority = 1000, -- Ensure it loads first
-		lazy = false, --
-		config = function()
-			vim.cmd("colorscheme onedark")
-		end,
-	}
-
+    {
+        "olimorris/onedarkpro.nvim",
+        priority = 1000, -- Ensure it loads first
+        lazy = false, --
+        config = function()
+            vim.cmd("colorscheme onedark")
+        end,
+    }
+}
