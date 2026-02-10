391484233220404ed95b696f415edc686e29caff
Bart Stavenuiter <spoiker@hotmail.com>
2026-02-10 10:08:01 +0100

    new treesitter setup setting up installed parsers in new way and enabling treesitter for these filetypes

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,40 +1,22 @@
 return {
     {
-        "nvim-treesitter/nvim-treesitter",
-        branch = 'master',
+        'nvim-treesitter/nvim-treesitter',
         lazy = false,
-        build = ":TSUpdate",
-        main = "nvim-treesitter.configs",
+        build = ':TSUpdate',
 
-        opts = {
+        init = function()
 
-            ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'php' },
+            require'nvim-treesitter'.install({ 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'php', 'go'})
 
-            indent = { enable = true },
-
-            highlight = {
-                enable = true,
-
-                -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-                -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-                -- the name of the parser)
-                -- list of language that will be disabled
-                -- disable = {},
-                -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-                disable = function(lang, buf)
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = require'nvim-treesitter'.get_installed() or {},
+                callback = function()
+                    -- syntax highlighting, provided by Neovim
+                    vim.treesitter.start()
+                    -- indentation, provided by nvim-treesitter
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
                 end,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-        }
+            })
+        end,
     }
 }
