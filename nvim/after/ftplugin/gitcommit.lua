dddb58619c8b84081d04a4ee193c0f528fd5e7bb
Bart Stavenuiter <spoiker@hotmail.com>
2026-02-12 13:24:29 +0100

    update: when committing set the right commit prefix

--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -1,3 +1,6 @@
 local branch = vim.fn.system('git branch --show-current')
-branch = branch:gsub("^(feature%/)", ''):gsub("^(%a+)-(%d+)(.*)$", '%1-%2')
-vim.api.nvim_put({'[' .. branch .. '] '}, 'c', false, true)
+local found_branch = branch:match("([A-Z]+-%d+)")
+branch = found_branch or ""
+if (branch ~= nil and branch ~= "") then
+  vim.api.nvim_put({ '[' .. branch .. '] ' }, 'c', false, true)
+end
