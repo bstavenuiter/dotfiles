02dfcc3ee61a9e38f6e9ee7f734c32cc6cfc1ee0
Bart Stavenuiter <b.stavenuiter@leads.io>
2026-07-14 13:45:17 +0200

    sidekick: switch to claude and add send-file/send-line keys

--- nvim/lua/plugins/sidekick-nvim.lua
+++ nvim/lua/plugins/sidekick-nvim.lua
@@ -2,7 +2,9 @@ return {
     {
         "folke/sidekick.nvim",
         keys = {
-            { "<leader>og", function () require("sidekick.cli").toggle("gemini") end, desc = "open sidekick"},
+            { "<leader>og", function () require("sidekick.cli").toggle("claude") end, desc = "Sidekick open"},
+            { "<leader>of", function () require("sidekick.cli").send({ msg = "{file}"}) end, desc = "Sidekick send file"},
+            { "<leader>ol", function () require("sidekick.cli").send({ msg = "{line}"}) end, desc = "Sidekick send line"},
         },
     },
 }
