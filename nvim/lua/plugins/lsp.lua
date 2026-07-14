66bfd5eeeb5ad9b970912bec9f35a6a222b2db07
Bart Stavenuiter <b.stavenuiter@leads.io>
2026-07-14 13:46:50 +0200

    lsp: add ts_ls with vue plugin, vtsls, and vue_ls servers

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -54,6 +54,23 @@ return {
                 },
                 { name = "pyright" },
                 {
+                    name = "ts_ls",
+                    setup = {
+                        init_options = {
+                            plugins = {
+                                {
+                                    name = "@vue/typescript-plugin",
+                                    location = vim.fn.trim(vim.fn.system("npm root -g")) .. "/@vue/language-server",
+                                    languages = { "vue" },
+                                },
+                            },
+                        },
+                        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+                    },
+                },
+                { name = "vtsls" },
+                { name = "vue_ls" },
+                {
                     name = "yamlls",
                     setup = {
                         settings = {
